f72b359d1853dd1f8e6e84891deec5071dbbb0ec
Rodrigo Silveira Mori <86813124+Red-Chot@users.noreply.github.com>
2024-07-30 16:34:02 -0300

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/Red-Chot/Aula03/internal/handler"
+	repository "github.com/Red-Chot/Aula03/internal/repositoy"
+	"github.com/Red-Chot/Aula03/internal/service"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// "postgresql://<username>:<password>@<database_ip>/todos?sslmode=disable"
+	dsn := "postgresql://postgres:postgres@localhost/go-simple-rpg-api?sslmode=disable"
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	playerRepository := repository.NewPlayerRepository(db)
+	playerService := service.NewPlayerService(*playerRepository)
+	playerHandler := handler.NewPlayerHandler(playerService)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /player", playerHandler.AddPlayer)
+	mux.HandleFunc("GET /player", playerHandler.LoadPlayers)
+	mux.HandleFunc("DELETE /player/{id}", playerHandler.DeletePlayer)
+	mux.HandleFunc("GET /player/{id}", playerHandler.LoadPlayer)
+	mux.HandleFunc("PUT /player/{id}", playerHandler.SavePlayer)
+
+	fmt.Println("Server is running on port 8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Println(err)
+	}
+}
